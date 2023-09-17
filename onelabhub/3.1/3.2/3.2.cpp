e1addb37b40ad2b9be8912d37be652de166bdd65
Semenov Alexander <Semenov Alexander>
2023-09-17 12:39:23 +0300

    Сделал все задания(проблемы в 3.2 и 5)

---
+++ onelabhub/3.1/3.2/3.2.cpp
@@ -0,0 +1,83 @@
+﻿// 3.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// Функция для вычисления НОД (наибольшего общего делителя)
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+// Функция для нахождения обратного элемента a по модулю M (a^(-1) mod M)
+int modInverse(int a, int M) {
+    for (int x = 1; x < M; x++) {
+        if ((a * x) % M == 1) {
+            return x;
+        }
+    }
+    return -1; // Обратного элемента нет
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int a, b, M;
+    cout << "Введите a: ";
+    cin >> a;
+    cout << "Введите b: ";
+    cin >> b;
+    cout << "Введите M: ";
+    cin >> M;
+
+    // Сложение (a + b) mod M
+    int sum = (a + b) % M;
+    cout << "(a + b) mod M = " << sum << endl;
+
+    // Вычитание (a - b) mod M
+    int diff = (a - b + M) % M;
+    cout << "(a - b) mod M = " << diff << endl;
+
+    // Умножение (a * b) mod M
+    int product = (a * b) % M;
+    cout << "(a * b) mod M = " << product << endl;
+
+    // Возведение в степень (a^b) mod M
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result = (result * a) % M;
+    }
+    cout << "(a^b) mod M = " << result << endl;
+
+    // Поиск обратного элемента a по модулю M (a^(-1) mod M)
+    int inverse_a = modInverse(a, M);
+    if (inverse_a != -1) {
+        cout << "a^(-1) mod M = " << inverse_a << endl;
+    }
+    else {
+        cout << "a^(-1) mod M = нет решения" << endl;
+    }
+
+    // Поиск обратного элемента b по модулю M (b^(-1) mod M)
+    int inverse_b = modInverse(b, M);
+    if (inverse_b != -1) {
+        cout << "b^(-1) mod M = " << inverse_b << endl;
+    }
+    else {
+        cout << "b^(-1) mod M = нет решения" << endl;
+    }
+
+    // Деление (a / b) mod M
+    if (gcd(b, M) == 1) {
+        int division = (a * modInverse(b, M)) % M;
+        cout << "(a / b) mod M = " << division << endl;
+    }
+    else {
+        cout << "(a / b) mod M = нет решения (b и M не взаимно просты)" << endl;
+    }
+
+    return 0;
+}
---
+++ onelabhub/4/4/4.cpp
@@ -0,0 +1,94 @@
+﻿// 4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Функция для вывода первых N простых чисел с использованием решета Эратосфена
+void printFirstNPrimes(int N) {
+    if (N <= 0) {
+        cout << "Введите положительное число N." << endl;
+        return;
+    }
+
+    vector<bool> isPrime(N * 10, true); // Используем вектор булевых значений для определения простых чисел
+    vector<int> primes;
+
+    for (int p = 2; p * p < N * 10; p++) {
+        if (isPrime[p]) {
+            for (int i = p * p; i < N * 10; i += p) {
+                isPrime[i] = false;
+            }
+        }
+    }
+
+    for (int p = 2; p < N * 10 && primes.size() < N; p++) {
+        if (isPrime[p]) {
+            primes.push_back(p);
+        }
+    }
+
+    cout << "Первые " << N << " простых чисел:" << endl;
+    for (int prime : primes) {
+        cout << prime << " ";
+    }
+    cout << endl;
+}
+
+// Функция для проверки числа на простоту
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    if (number <= 3) {
+        return true;
+    }
+    if (number % 2 == 0 || number % 3 == 0) {
+        return false;
+    }
+
+    for (int i = 5; i * i <= number; i += 6) {
+        if (number % i == 0 || number % (i + 2) == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    int choice, N, number;
+
+    cout << "Выберите действие:" << endl;
+    cout << "1. Вывести первые N простых чисел." << endl;
+    cout << "2. Проверить число на простоту." << endl;
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        cout << "Введите N: ";
+        cin >> N;
+        printFirstNPrimes(N);
+        break;
+    case 2:
+        cout << "Введите число для проверки на простоту: ";
+        cin >> number;
+        if (isPrime(number)) {
+            cout << number << " - простое число." << endl;
+        }
+        else {
+            cout << number << " - не является простым числом." << endl;
+        }
+        break;
+    default:
+        cout << "Неверный выбор." << endl;
+        break;
+    }
+
+    return 0;
+}
+
